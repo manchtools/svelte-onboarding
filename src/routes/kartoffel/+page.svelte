b5ca1a69e7924c6150c8320f0125ef639b4cfd08
Paul <paul.dotterer@armedangels.com>
2023-04-11 14:13:17 +0200

    added invalidate button

--- src/routes/kartoffel/+page.svelte
+++ src/routes/kartoffel/+page.svelte
@@ -2,6 +2,7 @@
 	import { onboardingSteps } from "$lib/onboarding";
 	import { onboardSteps } from "$lib/store";
 
+
 	onboardSteps.setSteps([
 		"Hello",
 		"Erbsen sind echt tolle sachen, man kann sie Braten kochen und sonstige sachen damit machen",
@@ -13,3 +14,6 @@
 
 <div style="margin-top: 20%;" use:onboardingSteps={1}>Kartoffeln</div>
 <div style="margin-top: 50%; margin-left:20%" use:onboardingSteps={2}>Erbsen</div>
+
+
+<button on:click={onboardSteps.invalidateRoute}>Invalidate Route</button>
