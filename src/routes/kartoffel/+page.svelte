5432ee6496f82949fc2135f41f4e71ea7f7ee34b
Paul <paul.dotterer@armedangels.com>
2023-04-11 13:48:15 +0200

    new test page

---
+++ src/routes/kartoffel/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onboardingSteps } from "$lib/onboarding";
+	import { onboardSteps } from "$lib/store";
+
+	onboardSteps.setSteps([
+		"Hello",
+		"Erbsen sind echt tolle sachen, man kann sie Braten kochen und sonstige sachen damit machen",
+		"Erbsen"
+	]);
+</script>
+
+<div use:onboardingSteps={0}>Hallo welt</div>
+
+<div style="margin-top: 20%;" use:onboardingSteps={1}>Kartoffeln</div>
+<div style="margin-top: 50%; margin-left:20%" use:onboardingSteps={2}>Erbsen</div>
