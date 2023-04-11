92b45af295d773f09b19bbd328aac538ed95456b
Paul <paul.dotterer@armedangels.com>
2023-04-11 14:44:39 +0200

    added long text

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -5,11 +5,11 @@
 	onboardSteps.setSteps([
 		"Hello",
 		"Erbsen sind echt tolle sachen, man kann sie Braten kochen und sonstige sachen damit machen",
-		"Erbsen"
+		"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimat"
 	]);
 </script>
 
 <div use:onboardingSteps={0}>Hallo welt</div>
 
 <div style="margin-top: 20%;" use:onboardingSteps={1}>Kartoffeln</div>
-<div style="margin-top: 50%; margin-left:20%" use:onboardingSteps={2}>Erbsen</div>
+<div style="margin-top: 90%; margin-left:20%" use:onboardingSteps={2}>Erbsen</div>
